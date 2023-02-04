6fd8fa4ece3e613049a42705fbb2e7478d997212
Diego Valles <dgvall134@gmail.com>
2023-02-04 16:57:44 -0700

    Created basic GET routes in application controller.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  get '/lists' do
+    lists = List.all
+    lists.to_json
+  end
+  get '/lists/:id' do
+    list = List.find(params[:id])
+    list.to_json
+  end
+  get '/lists/:id/sublists' do
+    sublists = List.find(params[:id]).sublists
+    sublists.to_json
+  end
+  get '/lists/:id/sublists/:sublist_name' do
+    sublists = List.find(params[:id]).sublists
+    sublist = sublists.find_by(name: params[:sublist_name])
+    sublist.to_json
+  end
 end
