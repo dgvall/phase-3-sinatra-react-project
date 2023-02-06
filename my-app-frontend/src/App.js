d20dee08b21d6e134ef85704a2dea4f3e6e11313
Diego Valles <dgvall134@gmail.com>
2023-02-05 20:55:53 -0700

    Created 2 routes: lists and lists/:id

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,11 +1,21 @@
 import React, {useState, useEffect} from "react"
 import {Switch, Route} from "react-router-dom"
+import Lists from "./Lists"
+import List from "./List"
 
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Switch>
+        <Route exact path = '/lists'>
+          <Lists />
+        </Route>
+        <Route exact path = '/lists/:id'>
+          <List />
+        </Route>
+      </Switch>
     </div>
   );
 }
