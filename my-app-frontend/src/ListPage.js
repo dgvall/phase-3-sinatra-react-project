2a16c44610da050fbf4cea3f9de5997dc153287e
Diego Valles <dgvall134@gmail.com>
2023-02-06 22:35:38 -0700

    Started basic formatting for list page

--- my-app-frontend/src/ListPage.js
+++ my-app-frontend/src/ListPage.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {useParams} from "react-router-dom"
 
+import "./ListPage.css"
+
 function ListPage() {
+  const [list, setList] = useState([])
   const {id} = useParams()
+
+  useEffect(() => {
+    fetch(`http://localhost:9292/lists/${id}`)
+      .then(res => res.json())
+      .then(data => setList(data))
+  }, [])
+
+  console.log(list)
+
   return (
     <div>
-      {id}
+      <div id = "list-header">
+        <p>{"<-"}</p>
+        <p>{list.name}</p>
+      </div>
+      <div>
+        <p>Add Task</p>
+        <input
+          placeholder = "Task here"
+        ></input>
+      </div>
+      
     </div>
   )
 }
