1752d7002930080775a61935400c9bf2efc5a866
Diego Valles <dgvall134@gmail.com>
2023-02-08 15:49:43 -0700

    Created post route in controller that also initializes connected sublists.

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  # get '/' do
-  #   list = List.find(17).tasks
-  #   list.to_json
-  # end
 
   # returns full nested data structure
   get '/lists' do
@@ -18,6 +14,17 @@ class ApplicationController < Sinatra::Base
     list = List.find(params[:id])
     list.to_json(include: { sublists: { include: :tasks} })
   end
+
+  # create new list and initialize sublists
+  post '/lists' do
+    list = List.create(name: params[:name])
+    sublists = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "General"]
+    sublists.each do |s|
+      Sublist.create(name: s, list_id: list.id)
+    end 
+    list.to_json
+  end
+
   # get '/lists/:id/sublists' do
   #   sublists = List.find(params[:id]).sublists
   #   sublists.to_json
