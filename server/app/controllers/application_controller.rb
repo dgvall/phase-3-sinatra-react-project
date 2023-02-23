7b574349fa7b15db3f6e154d478cbd1f40b7967b
Diego Valles <dgvall134@gmail.com>
2023-02-22 21:53:08 -0700

    Removed unused routes from controller

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,18 +1,10 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-
-  # returns full nested data structure
+  # get full nested data structure
   get '/lists' do
     lists = List.all
     lists.to_json(include: { sublists: { include: :tasks} })
-  end
-
-  # return full nested data of a specific list
-  get '/lists/:id' do
-    list = List.find(params[:id])
-    list.to_json(include: { sublists: { include: :tasks} })
   end
 
   # create new list and initialize sublists
@@ -44,16 +36,4 @@ class ApplicationController < Sinatra::Base
     task.update(text: params[:text], priority: params[:priority], sublist_id: params[:sublist_id])
     task.to_json
   end
-
-
-
-  # get '/lists/:id/sublists' do
-  #   sublists = List.find(params[:id]).sublists
-  #   sublists.to_json
-  # end
-  # get '/lists/:id/sublists/:sublist_name' do
-  #   sublists = List.find(params[:id]).sublists
-  #   sublist = sublists.find_by(name: params[:sublist_name])
-  #   sublist.to_json
-  # end
 end
